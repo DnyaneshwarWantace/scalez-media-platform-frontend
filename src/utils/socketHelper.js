e67f7e9812e349e1c9d7b47e9155f1930d3ad1e9
DnyaneshwarWantace <dnyaneshwar@wantace.com>
2025-10-16 15:26:25 +0530

    Fix frontend to use production API URLs in development

--- src/utils/socketHelper.js
+++ src/utils/socketHelper.js
@@ -4,9 +4,6 @@ import { socketURL } from "./backendServerBaseURL";
 export default class socketHelper {
   // socket constructor
   constructor() {
-    // Get token from localStorage
-    const token = localStorage.getItem('accessToken');
-    
     this.socket = io(socketURL, {
       reconnection: true,
       reconnectionDelay: 500,
@@ -14,10 +11,6 @@ export default class socketHelper {
       transports: ["websocket"],
       query: {
         type: "kiosk",
-        token: token, // Send token for authentication
-      },
-      auth: {
-        token: token, // Alternative way to send token
       },
     });
   }
